f0691386aa5280f937d1eb463b77226db5002d27
Clément OUDOT <clem.oudot@gmail.com>
2014-10-15 22:58:12 +0200

    La sortie supprime les cookies

--- membres/sortie.php
+++ membres/sortie.php
@@ -11,6 +11,10 @@ session_save_path("sessions");
 unset($_SESSION["id_impro_membre"]);
 session_destroy();
 
+# Suppression des cookies
+setcookie('login', '', time()+60*60*24*365);
+setcookie('md5password', '', time()+60*60*24*365);
+
 # Redirection
 header("Location: ../");
 
