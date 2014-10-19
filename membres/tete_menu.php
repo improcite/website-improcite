95f7783268c15c00e6749d81fff4fe8bde21f977
Clément OUDOT <clem.oudot@gmail.com>
2014-10-19 12:18:37 +0200

    Refonte du menu de l'espace membres

--- membres/tete_menu.php
+++ membres/tete_menu.php
@@ -1,41 +1,53 @@
-<?
-$aData = array(
-	array("membres", "membres.php", "Membres", "Liste des membres")
-	,array("infos", "infos.php", "Mes infos", "Modifier mes informations")
-	,array("reservation", "reservation.php", "R&eacute;servations", "")
-	//,array("fichiers", "fichiers.php", "Fichiers", "")
-	,array("dispos_s", "dispos.php", "Dispos spectacles", "")
-	,array("dispos_e", "dispos.php?train=1", "Dispos entrainements", "")
-	,array("dispos_st", "dispos_stats.php", "Statistiques", "")
-	,array("bdd", "admin_bdd.php", "Données", "Base de donn&eacute;es")
-	,array("sortie", "sortie.php", "Sortir", "Retour au site")
-	);
-
-?>
-
     <div id="menumembres" class="navbar-wrapper">
       <div class="container">
 
-        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+        <nav class="navbar navbar-inverse" role="navigation">
           <div class="container">
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="index.php">Accueil</a>
+              <a class="navbar-brand" href="index.php"><i class="glyphicon glyphicon-home"></i>  Accueil</a>
             </div>
             <div class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-			<? foreach($aData as $v) { ?>
-			<li class="<?=((basename($_SERVER['REQUEST_URI']) == $v[1])?'active':'')?>"><a href="<?=$v[1]?>"><?=$v[2]?></a></li>
-			<? } ?>
+                <li class="<?=((basename($_SERVER['REQUEST_URI']) == "membres.php")?'active':'')?>">
+                  <a href="membres.php"><i class="glyphicon glyphicon-th-list"></i> Membres</a>
+                </li>
+                <li class="<?=((basename($_SERVER['REQUEST_URI']) == "infos.php")?'active':'')?>">
+                  <a href="infos.php"><i class="glyphicon glyphicon-user"></i> Mon compte</a>
+                </li>
+                <li class="<?=((basename($_SERVER['REQUEST_URI']) == "reservation.php")?'active':'')?>">
+                  <a href="reservation.php"><i class="glyphicon glyphicon-shopping-cart"></i> R&eacute;servations</a>
+                </li>
+                <li class="dropdown <?=( strstr( basename($_SERVER['REQUEST_URI']), "dispos") ?'active':'')?>">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-calendar"></i> Disponibilit&eacute;s <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li class="<?=((basename($_SERVER['REQUEST_URI']) == "dispos.php")?'active':'')?>">
+                      <a href="dispos.php"><i class="glyphicon glyphicon-star"></i> Spectacles</a>
+                    </li>
+                    <li class="<?=((basename($_SERVER['REQUEST_URI']) == "dispos.php?train=1")?'active':'')?>">
+                      <a href="dispos.php?train=1"><i class="glyphicon glyphicon-star-empty"></i> Entra&icirc;nements</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="<?=((basename($_SERVER['REQUEST_URI']) == "dispos_stats.php")?'active':'')?>">
+                      <a href="dispos_stats.php"><i class="glyphicon glyphicon-stats"></i> Statistiques</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="<?=((basename($_SERVER['REQUEST_URI']) == "admin_bdd.php")?'active':'')?>">
+                  <a href="admin_bdd.php"><i class="glyphicon glyphicon-wrench"></i> Donn&eacute;es</a>
+                </li>
+              </ul>
+              <ul class="nav navbar-nav">
+                <li><a href="sortie.php"><i class="glyphicon glyphicon-log-out"></i> Sortir</a></li> 
               </ul>
             </div>
           </div>
-        </div>
+        </nav>
 
       </div>
     </div>
