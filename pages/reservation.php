d09930f78a2da1c05daa0c3148ed1796f917067a
Clément OUDOT <clem.oudot@gmail.com>
2015-09-06 21:15:18 +0200

    Affichage d'une seule image sur la page réservation

--- pages/reservation.php
+++ pages/reservation.php
@@ -140,7 +140,7 @@ else if ( $action == "Choisir" || $_REQUEST[ "id_spectacle" ] )
 	if ( file_exists($photoEvenement) ) {
 		echo "<img src=\"$photoEvenement\" class=\"affiche\" style=\"float:left;margin-right:20px;\"/>\n";
 	}
-	if ( file_exists($photoLieu) ) {
+	elseif ( file_exists($photoLieu) ) {
 		echo "<img src=\"$photoLieu\" class=\"affiche\" style=\"float:left;margin-right:20px;\"/>\n";
 	}	
 	elseif( file_exists($photoCategorie) ) {
