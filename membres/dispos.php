a125d34de479526228161d8d5f5ec86f94fadb67
Clément OUDOT <clement@oodo.net>
2024-07-16 22:21:43 +0200

    Avant / Après

--- membres/dispos.php
+++ membres/dispos.php
@@ -24,6 +24,34 @@ if ($_GET["month"] && $_GET["year"]) {
     $month = date("m");
 }
 
-$smarty->assign('month', $month);
-$smarty->assign('year', $year);
+$month_before = 0;
+$month_after = 0;
+$year_before = 0;
+$year_after = 0;
 
+if ($month === "01") {
+	$month_before = "12";
+	$month_after = "02";
+	$year_before = intval($year) - 1;
+	$year_after = intval($year);
+} elseif ($month === "12") {
+	$month_before = "11";
+	$month_after = "01";
+	$year_before = intval($year);
+	$year_after = intval($year) + 1;
+} else {
+	$month_before = intval($month) -1;
+	$month_after = intval($month) + 1;
+	$year_before = intval($year);
+	$year_after = intval($year);
+}
+
+if ( strlen( $month_before ) == 1) { $month_before = "0" . $month_before; }
+if ( strlen( $month_after ) == 1) { $month_after = "0" . $month_after; }
+
+$smarty->assign('month', $month);
+$smarty->assign('month_before', $month_before);
+$smarty->assign('month_after', $month_after);
+$smarty->assign('year', $year);
+$smarty->assign('year_before', $year_before);
+$smarty->assign('year_after', $year_after);
--- templates/membres/dispos.tpl
+++ templates/membres/dispos.tpl
@@ -1,6 +1,10 @@
 <div class="clearfix">
   <h3 class="float-start"><i class="fa fa-calendar-check me-3"></i>Disponibilités</h3>
-  <button class="btn btn-secondary float-end">{$month} / {$year}</button>
+  <div class="btn-group float-end" role="group">
+    <a href="index.php?p=dispos&month={$month_before}&year={$year_before}" class="btn btn-secondary"><i class="fa-solid fa-caret-left"></i></a>
+    <button type="button" class="btn btn-secondary">{$month} / {$year}</button>
+    <a href="index.php?p=dispos&month={$month_after}&year={$year_after}" class="btn btn-secondary"><i class="fa-solid fa-caret-right"></i></a>
+  </div>
 </div>
 <hr />
 
