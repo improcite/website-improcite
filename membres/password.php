d5d62c6cb2fd861df679c8a97a69d8abba89741e
Clément OUDOT <clement@oodo.net>
2024-08-25 13:19:24 +0200

    Suppression cookie lors du changement de mot de passe

--- membres/password.php
+++ membres/password.php
@@ -10,6 +10,7 @@ if ($_POST["password"]) {
         $update = updatePassword($mysqli, $table_comediens, $membre["id"], $_POST["password"], $salt);
         if ($update) {
             $result = "passwordchanged";
+            setcookie('md5password', '', time()+60*60*24*365);
         } else {
             $result = "passwordnotchanged";
         }
