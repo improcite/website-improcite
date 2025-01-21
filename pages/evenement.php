5baec5a1c136ad59a3c787c214bff9160b2e8c6f
Clément OUDOT <clement@oodo.net>
2025-01-21 23:03:30 +0100

    Pas de balises HTML dans la description opengraph

--- pages/evenement.php
+++ pages/evenement.php
@@ -17,5 +17,5 @@ if (preg_match('/^(;)+$/', $infos['animateurs'])) { $infos['animateurs'] = null;
 
 $smarty->assign('infos', $infos);
 $smarty->assign('og_title', "Improcité - ".$infos['nom']." - ".$infos['lnom']);
-$smarty->assign('og_description', $infos['ecommentaire'] ? $infos['ecommentaire'] : $infos['description']);
+$smarty->assign('og_description', $infos['ecommentaire'] ? strip_tags($infos['ecommentaire']) : strip_tags($infos['description']));
 $smarty->assign('og_image', "https://improcite.com".$infos['photo']);
